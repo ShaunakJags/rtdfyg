26d04e447fb1e5da2054bed67670cc9a9bbd294a
ShaunakJags <96124310+ShaunakJags@users.noreply.github.com>
2023-03-28 11:43:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function setup() {
+    video = createCapture(VIDEO);
+    video.size(500,500);
+
+    canvas = createCanvas(500,500);
+    canvas.position(560,150);
+
+    posenet = ml5.poseNet(video, modelLoaded);
+    posenet.on("pose" , gotPoses);
+}
+
+function modelLoaded() {
+    console.log("Model is loaded");
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+    }
+}
